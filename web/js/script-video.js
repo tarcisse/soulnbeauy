d796643ed043afb99dd8aef15e0a946bf8132382
Zigabe jos <jos.zigabe@uclouvain.student.be>
2015-11-08 19:33:37 +0100

    agency slider3

--- web/js/script-video.js
+++ web/js/script-video.js
@@ -7,15 +7,28 @@ $(document).ready(function () {
 	
 });
 
+
+
 //AGENCY slider 
 $(function() {
     $('.banner').unslider({
 		speed: 500,               //  The speed to animate each slide (in milliseconds)
-		delay: 3000,              //  The delay between slide animations (in milliseconds)
+		delay: 10000,              //  The delay between slide animations (in milliseconds)
 		complete: function() {},  //  A function that gets called after every slide animation
 		keys: true,               //  Enable keyboard (left, right) arrow shortcuts
 		dots: true,               //  Display dot navigation
 		fluid: false              //  Support responsive design. May break non-responsive designs	
+	});
+	
+	var slides = jQuery('.banner'),
+    i = 0;
+
+	slides
+	.on('swipeleft', function(e) {
+	  slides.eq(i + 1).addClass('active');
+	})
+	.on('swiperight', function(e) {
+	  slides.eq(i - 1).addClass('active');
 	});
 });
 
